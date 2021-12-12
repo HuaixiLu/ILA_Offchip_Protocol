d6718bcd43d35dc3aa2c4f144125615e48daaab0
Huaixi Lu <huaixil@princeton.edu>
2021-12-12 14:43:12 -0500

    bsg_offchip added

---
+++ bsg_ocp_IV/HLIV/hliv.tcl
@@ -0,0 +1,18 @@
+analyze -sva  \
+    top.v \
+    bsg_link_ddr_downstream.sv2v.v\
+    bsg_link_ddr_upstream.sv2v.v
+
+elaborate -top bsg_top
+clock base_clk;
+clock base_clk io_clk 2 1 -both_edges;
+clock base_clk core_clk 4 1
+clock base_clk edge_clk_i 4 4;
+reset -expression {rst} {:global_formal_reset} -non_resettable_regs {0};
+
+assume {~core_yumi_i || valid_o}
+assume {~valid_o || core_yumi_i}
+
+assert {##10 edge_clk[0] == edge_clk_i}
+assert {edge_clk[0] == edge_clk[1]}
+assert {@(posedge core_clk) (valid_i & upstream.core_ready_o) |=> ##[0:$] valid_o}
---
+++ bsg_ocp_SEC/verification/spec.tcl
@@ -0,0 +1,13 @@
+analyze -sva  \
+    off-chip-specification.v
+
+elaborate -top spec
+clock clk
+reset rst
+
+assume {@(posedge clk) ((down_wptr != down_rptr) | (valid_out == 1)) |-> ##[0:$] ready}
+
+assert {@(posedge clk) valid_in |=> ##[0:$] valid_out}
+assert {req |=> ##[0:$] ack}
+assert {req_x |=> ##[0:$] ack_x}
+assert {counter < 8}
